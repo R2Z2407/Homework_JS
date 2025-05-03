fd30175b80d96a2876031ae42ad721abb39f5daf
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2025-05-03 11:53:48 +0700

    Update script.js

--- HW1/script.js
+++ HW1/script.js
@@ -1,18 +1,18 @@
 function upDate(previewPic) {  
-  const imageDiv = document.getElementById("image");  
-  imageDiv.innerHTML = previewPic.alt;  
+    const imageDiv = document.getElementById("image");  
+    imageDiv.innerHTML = previewPic.alt;  
 
-  const largeImageUrl = previewPic.getAttribute('data-large');  
-  if (largeImageUrl) {  
-      imageDiv.style.backgroundImage = 'url("' + largeImageUrl + '")';  
-  } else {  
-      imageDiv.style.backgroundImage = 'none';
-  }  
-  imageDiv.style.backgroundSize = 'cover';   
+    // Ambil URL gambar besar dari data-large  
+    const largeImageUrl = previewPic.getAttribute('data-large');  
+    if (largeImageUrl) {  
+        imageDiv.style.backgroundImage = 'url("' + largeImageUrl + '")';  
+    } else {  
+        imageDiv.style.backgroundImage = 'none';  
+    }  
 }  
 
 function unDo() {  
-  const imageDiv = document.getElementById("image");  
-  imageDiv.innerHTML = "Hover over an image below to display here.";  
-  imageDiv.style.backgroundImage = 'none';  
-}  
+    const imageDiv = document.getElementById("image");  
+    imageDiv.innerHTML = "Hover over an image below to display here.";  
+    imageDiv.style.backgroundImage = 'none';  
+}
