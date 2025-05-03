96330e726162076e536d4ec0fb1476db486471f0
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2025-05-03 11:26:32 +0700

    Update script.js

--- HW1/script.js
+++ HW1/script.js
@@ -1,12 +1,18 @@
 function upDate(previewPic) {  
-  const imageDiv = document.getElementById("image");
-  imageDiv.innerHTML = previewPic.alt;
-  imageDiv.style.backgroundImage = 'url("' + previewPic.src +'")';
-  imageDiv.style.backgroundSize = 'cover'; 
+  const imageDiv = document.getElementById("image");  
+  imageDiv.innerHTML = previewPic.alt;  
+
+  const largeImageUrl = previewPic.getAttribute('data-large');  
+  if (largeImageUrl) {  
+      imageDiv.style.backgroundImage = 'url("' + largeImageUrl + '")';  
+  } else {  
+      imageDiv.style.backgroundImage = 'none';
+  }  
+  imageDiv.style.backgroundSize = 'cover';   
 }  
 
 function unDo() {  
   const imageDiv = document.getElementById("image");  
-  imageDiv.innerHTML = "Hover over an image below to display here.";
-  imageDiv.style.backgroundImage = 'none';
+  imageDiv.innerHTML = "Hover over an image below to display here.";  
+  imageDiv.style.backgroundImage = 'none';  
 }  
