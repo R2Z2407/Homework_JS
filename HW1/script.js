8bf50eae450774a487f3389a4f6337bf8f45c70d
R2Z2407 <90142366+R2Z2407@users.noreply.github.com>
2025-05-05 06:28:15 +0700

    Add files via upload

--- HW1/script.js
+++ HW1/script.js
@@ -1,18 +1,12 @@
 function upDate(previewPic) {  
-    const imageDiv = document.getElementById("image");  
-    imageDiv.innerHTML = previewPic.alt;  
-
-    // Ambil URL gambar besar dari data-large  
-    const largeImageUrl = previewPic.getAttribute('data-large');  
-    if (largeImageUrl) {  
-        imageDiv.style.backgroundImage = 'url("' + largeImageUrl + '")';  
-    } else {  
-        imageDiv.style.backgroundImage = 'none';  
-    }  
+  const imageDiv = document.getElementById("image");
+  imageDiv.innerHTML = previewPic.alt;
+  imageDiv.style.backgroundImage = 'url("' + previewPic.src +'")';
+  imageDiv.style.backgroundSize = 'cover'; 
 }  
 
 function unDo() {  
-    const imageDiv = document.getElementById("image");  
-    imageDiv.innerHTML = "Hover over an image below to display here.";  
-    imageDiv.style.backgroundImage = 'none';  
-}
+  const imageDiv = document.getElementById("image");  
+  imageDiv.innerHTML = "Hover over an image below to display here.";
+  imageDiv.style.backgroundImage = 'none';
+}  
